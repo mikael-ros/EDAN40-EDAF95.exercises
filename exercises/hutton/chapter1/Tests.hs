e70a6dbeea36056f4e8483ff97661e0f767bcbad
Mikael <112403418+mikael-ros@users.noreply.github.com>
2024-03-29 23:45:07 +0100

    Fixed ambigous function call in Tests.hs

--- exercises/hutton/chapter1/Tests.hs
+++ exercises/hutton/chapter1/Tests.hs
@@ -5,7 +5,7 @@ module Tests where
     import Exercise4
     import Exercise5
 
-    testExercise3 = TestCase (assertEqual 24 product [2..4])
+    testExercise3 = TestCase (assertEqual 24 Exercise3.product [2..4])
     testExercise4 = TestCase (assertEqual [8,5,2,1] Exercise4.qsort [2,5,8,1]) 
     testExercise5 = TestCase (assertEqual [1,2,3] Exercise5.qsort [2,2,3,1,1]) 
     tests = TestList [TestLabel "testExercise3" testExercise3, 
